428dd4d800a045fcf4846545c08525ceaf094871
ShashwatsahniSs <shashwatsahni178@gmail.com>
2025-06-29 17:36:01 +0530

    Second push

--- demo.py
+++ demo.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+from tkinter import*
+root=Tk()
+
+def myClick():
+    myLabel=Label(root, text="Hi there!")
+    myLabel.pack()
+
+myButton=Button(root, text="Click here", command=myClick)
+myButton.pack()
+
+#label1=Label(root,text="Hello, World!")
+#label1.grid(row=0,column=0)
+
+#label2=Label(root,text="Welcome to the demo application!")
+#label2.grid(row=1,column=0)
+
+root.mainloop()
